5889ef3d32c71d1eee91a74984fa5ef097e559ba
Evgeny Zhigarin Windows <e.zhigarin@yandex.ru>
2024-06-15 13:41:52 +0300

    применил isort

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,9 +1,8 @@
 from http import HTTPStatus
+
 import pytest
-
-from pytest_django.asserts import assertRedirects, assertFormError
-
 from django.urls import reverse
+from pytest_django.asserts import assertFormError, assertRedirects
 
 from news.forms import BAD_WORDS, WARNING
 from news.models import Comment
